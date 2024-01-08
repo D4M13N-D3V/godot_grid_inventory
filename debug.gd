76ab6f7e028a06641f8d590c1f5fb507fb756e7f
Damien Ostler <damien.ostler@data443.com>
2024-01-07 21:43:28 -0500

    fix updated the debug scene so that the items are dynamically loaded

--- debug.gd
+++ debug.gd
@@ -1,6 +1,18 @@
 extends Node
 
 
+@export var inventory:InventoryController
+@export var grid:GridContainer
+func _ready():
+	var items = ItemDb.ITEMS
+	for item in items:
+		var button = Button.new()
+		grid.add_child(button)
+		button.text = "Spawn "+item
+		button.connect("pressed", func():
+			inventory.pickup_item(item))
+
+
 func _on_button_3_pressed():
 	$"..".pickup_item("small_debug_item")
 
